e4c35a51bef94e1480f638fb3959c5d8c643c702
Ubuntu <ubuntu@ip-172-31-91-136.ec2.internal>
2023-12-19 23:31:17 +0000

    Todo

---
+++ scripts/install_lemp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Mostrar vervose
+set -ex
+
+# Incorporamos variables.
+source .env
+
+# Actualizamos los repositorios.
+apt update
+
+# Instalamos nginx
+apt install nginx -y
+
+# Instalar mysqlserver
+apt install mysql-server -y
+
+# Instalacion de paquetes: paquete php-fpm y php-mysql
+apt install php-fpm php-mysql -y
+
+# Copiamos el contenido de default de default a /etc/nginx/sites-available/
+cp ../conf/default /etc/nginx/sites-available/default
+
+# Reiniciamos el servicio de nginx
+systemctl restart nginx
+
+# Damos permisos al usuario de nginx
+chown -R www-data:www-data /var/www/html
+
+# Copiamos el contenido de php a html para comprobar que funciona.
+cp ../php/info.php /var/www/html
